4dc3803e5acf652ccd59d7cc0a269d01592bd4c8
98k <370103326@qq.com>
2019-07-21 21:51:29 +0800

    init repo

---
+++ src/test/java/io/anti98k/community/demo/Proj1ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.anti98k.community.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Proj1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
